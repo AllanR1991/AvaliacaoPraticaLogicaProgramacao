c7eec4ace37c732e040e86facf6cc16f622e18f7
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-08-11 21:48:40 -0300

    Atividade Finalizada

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Escreva um programa que peça ao usuário para digitar um número inteiro e informe se o número é par ou ímpar. Utilize uma estrutura condicional if/else para realizar o teste.
+
+int numeroLido = 0;
+
+bool resultTry;
+
+Console.WriteLine("Digite um número: ");
+
+do{
+
+    resultTry = int.TryParse(Console.ReadLine(), out numeroLido);
+    
+    if(!resultTry)
+    {
+        Console.WriteLine("Erro: valor inserido não conrresponde a um número.");
+    }
+    else if (numeroLido % 2 == 0)
+    {
+        Console.WriteLine($"O número digitado {numeroLido} é par.");
+    }
+    else
+    {
+        Console.WriteLine($"O número digitado {numeroLido} é impar.");
+    }
+
+}while(!resultTry);
+
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numeros = [1,2,3,4,5,6,7,8,9,10];
+int soma = 0;
+
+foreach(int numero in numeros){
+    if(numero % 2 == 0)
+    {
+        soma += numero;
+    }
+}
+
+Console.WriteLine($"O valor total da soma dos pares é : {soma} ");
